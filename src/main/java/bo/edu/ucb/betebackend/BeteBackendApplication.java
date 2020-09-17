c100f9ee8f42a9a1f126059ccb2fbd56aff4d7bf
Adam Quiroz <adam123quiroz@gmail.com>
2020-09-16 20:08:34 -0400

    Init Commit

---
+++ src/main/java/bo/edu/ucb/betebackend/BeteBackendApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.betebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/betebackend/BeteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.betebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
